f8b663b67add4c9af149bb6646f2cb535aeb485b
Mac Anh <anh@bespokify.com>
2023-04-24 13:49:02 +0700

    feat: slack

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 const core = require('@actions/core');
 const fetch = require('node-fetch');
 
-try {
-  const webhook = core.getInput('slack_webhook')
-  const manifest = JSON.parse(core.getInput('manifest'));
+const start = async function () {
+  try {
+    const webhook = core.getInput('slack_webhook')
+    const manifest = JSON.parse(core.getInput('manifest'));
 
-  const summary = manifest.map((it) => {
-    return [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Page: ${it.url}`
-        }
+    const summary = manifest.map((it) => {
+      return [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Page: ${it.url}`
+          }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Performance*: ${it.summary.performance}, *Accessibility*: ${it.summary.accessibility}, *Best Practices*: ${it.summary["best-practices"]} , *SEO*: ${it.summary.seo}, *PWA*: ${it.summary.pwa}`
+          }
+        },
+        {
+          type: "divider"
+        },
+      ]
+    }).flatMap((it) => [...it]);
+
+    const slackBody = { blocks: summary }
+
+    const resp = await fetch(webhook, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `*Performance*: ${it.summary.performance}, *Accessibility*: ${it.summary.accessibility}, *Best Practices*: ${it.summary["best-practices"]} , *SEO*: ${it.summary.seo}, *PWA*: ${it.summary.pwa}`
-        }
-      },
-      {
-        type: "divider"
-      },
-    ]
-  }).flatMap((it) => [...it]);
+      body: JSON.stringify(slackBody),
+    });
 
-  const slackBody = { blocks: summary }
-
-  const resp = await fetch(webhook, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(slackBody),
-  });
-
-  console.log(resp);
-} catch (error) {
-  core.setFailed(error.message);
+    console.log(resp);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+start();

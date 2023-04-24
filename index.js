87c5dffa5706349cf4f591a42e11d6593649114a
Mac Anh <anh@bespokify.com>
2023-04-24 13:43:28 +0700

    feat: init

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fetch = require('node-fetch');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const webhook = core.getInput('slack_webhook')
+  const manifest = JSON.parse(core.getInput('manifest'));
+
+  const summary = manifest.map((it) => {
+    return [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Page: ${it.url}`
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*Performance*: ${it.summary.performance}, *Accessibility*: ${it.summary.accessibility}, *Best Practices*: ${it.summary["best-practices"]} , *SEO*: ${it.summary.seo}, *PWA*: ${it.summary.pwa}`
+        }
+      },
+      {
+        type: "divider"
+      },
+    ]
+  }).flatMap((it) => [...it]);
+
+  const slackBody = { blocks: summary }
+
+  const resp = await fetch(webhook, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(slackBody),
+  });
+
+  console.log(resp);
 } catch (error) {
   core.setFailed(error.message);
 }
